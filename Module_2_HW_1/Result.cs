35090ea9ab018b6e23bf21e7e0f592eb465d3958
Alex <Alex@DESKTOP-S50F2GU>
2023-05-29 21:10:41 +0200

    Add project files.

---
+++ Module_2_HW_1/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_HW_1
+{
+    public class Result
+    {
+        public string Message { get; }
+
+        public bool Status { get; }
+
+        public Result(bool status, string message)
+        {
+        Status = status;
+        Message = message;
+        }
+    }
+}
